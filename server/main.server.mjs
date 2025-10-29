1e989b2b88cdbb7dea177ed8a62749c4a745509b
Your Name <youremail@example.com>
2025-10-29 22:47:34 +0300

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WFP4HEZC.mjs";import"./chunk-KQCBERWQ.mjs";import"./chunk-4KRFWWIY.mjs";import"./chunk-5BJTEH4I.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
